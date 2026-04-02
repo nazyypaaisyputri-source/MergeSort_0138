6698c91ad97819695023e396fcf411896029e585
nazyypaaisyputri-source <nazyypaaisyputri@gmail.com>
2026-04-02 18:02:10 +0700

    Implementasi perbandingan elemen kiri dan kanan

--- MergeSort_0138.cpp
+++ MergeSort_0138.cpp
@@ -41,6 +41,25 @@ void mergeSort(int low, int high)
 
     mergeSort(low, mid);
     mergeSort(mid + 1, high);
+
+    int i = low;
+    int j = mid + 1;
+    int k = low;
+
+    while (i <= mid && j <= high) 
+    {
+        if (arr[i] <= arr[j]) 
+        {
+            B[k] = arr[i];
+            i++;
+        } 
+        else 
+        {
+            B[k] = arr[j];
+            j++;
+        }
+        k++;
+    }
 }
 
 int main() {
